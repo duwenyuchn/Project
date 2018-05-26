e463c3db57bb386ad1c5305f89d902d974aea4f5
duwenyu <duwenyuchn@gmail.com>
2018-05-27 01:25:23 +0800

    增加注释

--- Mailapplication/urlSendmail.py
+++ Mailapplication/urlSendmail.py
@@ -8,7 +8,7 @@ password = input('Password: ')
 # 输入收件人地址:
 to_addr = input('To: ')
 # 输入SMTP服务器地址:
-smtp_server = input('SMTP server: ')
+smtp_server = input('SMTP server: ') # 腾讯qq邮箱smtp端口是smtp.qq.com
 server = smtplib.SMTP_SSL(smtp_server, 465)
 # server.set_debuglevel(1)
 server.login(from_addr, password)
